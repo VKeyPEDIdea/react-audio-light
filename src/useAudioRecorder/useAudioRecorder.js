0a03b1985acb7623e00aadb88c7d45bb3c09ebf4
Pavel Karyapkin <karyapkin@gmail.com>
2025-08-11 18:21:55 +0500

    Replace useState with useRef for MediaRecorder (#1)
    * 0.1.3
    
    * replace useState with useRef for media recorder to avoid rerender
    
    * 0.1.4

--- src/useAudioRecorder/useAudioRecorder.js
+++ src/useAudioRecorder/useAudioRecorder.js
@@ -1,28 +1,30 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const useAudioRecorder = () => {
   const [isRecording, setIsRecording] = useState(false);
   const [audioBlob, setAudioBlob] = useState(null);
-  const [mediaRecorder, setMediaRecorder] = useState(
-    null
-  );
+  const mediaRecorder = useRef(null);
 
   const startRecording = async () => {
-    if (!mediaRecorder) {
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-      const recorder = new MediaRecorder(stream);
-      recorder.ondataavailable = (event) => {
-        setAudioBlob(event.data);
-      };
-      recorder.start();
-      setMediaRecorder(recorder);
+    if (!mediaRecorder.current) {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        const recorder = new MediaRecorder(stream);
+        recorder.ondataavailable = (event) => {
+          setAudioBlob(event.data);
+        };
+        recorder.start();
+        mediaRecorder.current = recorder;
+        setIsRecording(true);
+      } catch (err) {
+        console.error('Error starting recording: ', err);
+      }
     }
-    setIsRecording(true);
   };
 
   const stopRecording = () => {
-    if (mediaRecorder) {
-      mediaRecorder.stop();
+    if (mediaRecorder.current) {
+      mediaRecorder.current.stop();
       setIsRecording(false);
     }
   };
